ba7039505eccf98693194cda94c9e51db8eca0ba
Fulgurance <zohran.londais@gmail.com>
2024-01-10 23:43:18 +0000

    Updated entry

--- Root/0.1.0/0.1.0.cr
+++ Root/0.1.0/0.1.0.cr
@@ -1,6 +1,6 @@
 class Target < ISM::SystemGroupSoftware
 
-    def initializeGroup(name: "Root", id: 0, defaultUser: "Root")
+    def initializeGroup(name: "Root", id: 0, defaultUserName: "Root")
     end
 
 end
