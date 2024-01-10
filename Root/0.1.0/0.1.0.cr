0ceb2585b2e9b5eda253c21bfc8d90c4697e7a5a
Fulgurance <zohran.londais@gmail.com>
2024-01-10 23:38:55 +0000

    Patched installer for root group

--- Root/0.1.0/0.1.0.cr
+++ Root/0.1.0/0.1.0.cr
@@ -1,6 +1,6 @@
 class Target < ISM::SystemGroupSoftware
 
-    def initializeGroup(name: "Root", id: 0, isUserGroup: true)
+    def initializeGroup(name: "Root", id: 0, defaultUser: "Root")
     end
 
 end
