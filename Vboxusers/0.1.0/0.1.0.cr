10af54efb1fd49c4dfbfd7acc8c5c6b9f830e7aa
Fulgurance <zohran.londais@gmail.com>
2024-06-06 22:17:16 +0100

    Added group vboxusers

---
+++ Vboxusers/0.1.0/0.1.0.cr
@@ -0,0 +1,15 @@
+class Target < ISM::VirtualSoftware
+
+    def install
+        super
+
+        runGroupAddCommand(["-fg","304","vboxusers"])
+    end
+
+    def uninstall
+        super
+
+        runGroupDelCommand(["-f","vboxusers"])
+    end
+
+end
